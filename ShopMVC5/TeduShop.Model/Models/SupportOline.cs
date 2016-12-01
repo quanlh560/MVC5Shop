a4a16b88c9032b5ff5d7bb7c40502f4d433d1529
DELLLK\m4600nvidia <haiquank56cn@gmail.com>
2016-12-01 21:04:49 +0700

    First Commit

---
+++ ShopMVC5/TeduShop.Model/Models/SupportOline.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("SupportOnlines")]
+    public class SupportOline
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { set; get; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { set; get; }
+
+        [MaxLength(256)]
+        public string Department { set; get; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Email { set; get; }
+
+        [MaxLength(50)]
+        public string Skype { set; get; }
+
+        public bool? Status { set; get; }
+    }
+}
---
+++ ShopMVC5/TeduShop.Model/Models/SystemConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("SystemConfigs")]
+    public class SystemConfig
+    {
+        [Key]
+        [MaxLength(50)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { set; get; }
+
+        [MaxLength(256)]
+        public string Code { set; get; }
+
+        public string valueString { set; get; }
+        public int ValueInt { set; get; }
+    }
+}
